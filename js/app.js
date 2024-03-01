fa0505c7e9e4b14f30dbe1cd9b8e348063a43f17
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-03-01 15:24:06 +0100

    Add: exercise-2 AND fix var es-1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 //ESERCIZIO-1
-const number = 100; //number
-console.log(number)
+// let number = 100; //number
+// console.log(number)
+
+
+//ESERCIZIO-2/2-b/2-c
+// const PI = '3.14' ;//string
+// console.log(PI)
+
+// ES 2-B
+const PI = '3.1416'; //string
+
+// ES 2-C
+let radius = 8; //number
+const product = radius * parseInt(PI) * 2;//number
+let circle = product;//number
+
+
+console.log(circle)
+// console.log(product,PI)
